a665e36594c413a36d5f8f39bd0913bddbad8d62
Arpitadey88 <deyarpitabanik@gmail.com>
2021-08-21 21:35:35 +0600

    added new

--- js/main.js
+++ js/main.js
@@ -1,30 +1,66 @@
 const singleMemory = document.getElementById("single-memory");
-singleMemory.addEventListener('click', function(){
-    price.innerText = "0";
+singleMemory.addEventListener('click', function () {
+    memory.innerText = "10";
+    updateTotalPrice();
 });
 const doubleMemory = document.getElementById("double-memory");
-doubleMemory.addEventListener('click', function(){
-    price.innerText = "180";
+doubleMemory.addEventListener('click', function () {
+    memory.innerText = "180";
+    updateTotalPrice();
 });
 
 const singleStorage = document.getElementById("single-ssd");
-singleStorage.addEventListener('click', function(){
-    pricessd.innerText = "0";
+singleStorage.addEventListener('click', function () {
+    storage.innerText = "10";
+    updateTotalPrice();
 });
 const doubleStorage = document.getElementById("double-ssd");
-doubleStorage.addEventListener('click', function(){
-    pricessd.innerText = "100";
+doubleStorage.addEventListener('click', function () {
+    storage.innerText = "100";
+    updateTotalPrice();
 });
 const tripleStorage = document.getElementById("triple-ssd");
-tripleStorage.addEventListener('click', function(){
-    pricessd.innerText = "180";
+tripleStorage.addEventListener('click', function () {
+    storage.innerText = "180";
+    updateTotalPrice();
 });
 
 const freeDelivery = document.getElementById("free-delivery");
-freeDelivery.addEventListener('click', function(){
-    cost.innerText = "0";
+freeDelivery.addEventListener('click', function () {
+    shipping.innerText = "10";
+    updateTotalPrice();
 });
 const urgentDelivery = document.getElementById("urgent-delivery");
-urgentDelivery.addEventListener('click', function(){
-    cost.innerText = "20";
+urgentDelivery.addEventListener('click', function () {
+    shipping.innerText = "20";
+    updateTotalPrice();
+});
+
+const defaultValue = document.getElementById("best");
+best.innerText = "1299";
+
+const totalPrice = document.getElementById("total-price");
+const grandTotal = document.getElementById("grand-total");
+const applyPromo = document.getElementById("apply");
+
+function updateTotalPrice() {
+    const defaultPrice = Number(best.innerText)
+    const memoryPrice = Number(memory.innerText);
+    const storagePrice = Number(storage.innerText);
+    const shippingPrice = Number(shipping.innerText);
+    const totalValue = defaultPrice + memoryPrice + storagePrice + shippingPrice;
+    totalPrice.innerText = totalValue;
+    grandTotal.innerText = totalValue;
+    // console.log(totalValue);
+    return totalValue;
+}
+// apply promocode for discount price
+applyPromo.addEventListener('click', function () {
+    let inputField = document.getElementById("input-value").value;
+    if (inputField == 'stevekaku') {
+        // console.log('grnad previous', grandTotal)
+        const grandTotalPrice = parseInt(updateTotalPrice() - (updateTotalPrice() * 20 / 100));
+        document.getElementById("grand-total").innerText = grandTotalPrice;
+        inputField = '';
+    }
 });
