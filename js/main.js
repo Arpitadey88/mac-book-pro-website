0a70aa7389f091eed6c606e3ed4c06313c4692f6
Arpitadey88 <deyarpitabanik@gmail.com>
2021-08-21 07:17:58 +0600

    added something new

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log('mac-book-pro from js');
+const singleMemory = document.getElementById("single-memory");
+singleMemory.addEventListener('click', function(){
+    price.innerText = "0";
+});
+const doubleMemory = document.getElementById("double-memory");
+doubleMemory.addEventListener('click', function(){
+    price.innerText = "180";
+});
+
+const singleStorage = document.getElementById("single-ssd");
+singleStorage.addEventListener('click', function(){
+    pricessd.innerText = "0";
+});
+const doubleStorage = document.getElementById("double-ssd");
+doubleStorage.addEventListener('click', function(){
+    pricessd.innerText = "100";
+});
+const tripleStorage = document.getElementById("triple-ssd");
+tripleStorage.addEventListener('click', function(){
+    pricessd.innerText = "180";
+});
+
+const freeDelivery = document.getElementById("free-delivery");
+freeDelivery.addEventListener('click', function(){
+    cost.innerText = "0";
+});
+const urgentDelivery = document.getElementById("urgent-delivery");
+urgentDelivery.addEventListener('click', function(){
+    cost.innerText = "20";
+});
